2384615976c2d1af2c882a9d181d126019ea9d2c
enzhili <enzhililsu@gmail.com>
2016-12-06 21:32:06 -0600

    Redefine the kernel to be consistent with normal convections

--- kernel.py
+++ kernel.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def K_real(omega_n, omega):
-    return -omega/(omega_n**2 + omega**2)*1/(2*np.pi)
+    return -omega/(omega_n**2 + omega**2)
 
 def K_imag(omega_n, omega):
-    return -omega_n/(omega_n**2 + omega**2)*1.0/(2*np.pi)
+    return -omega_n/(omega_n**2 + omega**2)
